dd8d3c6c7ca0ef5a24b5ac8e48886a943e7f348a
Bern <bernnystrom@gmail.com>
2022-08-18 15:11:37 +0800

    🏗️ (new): Add view model layer for publishing app state changes
    Added the app view model for publishing changes in the app state so that the main view screen can respond & display the updated changes to the user. In this commit, the view model is simply publishing any changes to whether the framework detail view is showing.

---
+++ AppleFrameworks/FrameworkGridViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridViewModel.swift
+//  AppleFrameworks
+//
+//  Created by Bern N on 8/18/22.
+//
+
+import SwiftUI
+
+final class FrameworkGridViewModel: ObservableObject {
+    @Published var isShowingDetailView = false
+    
+    var selectedFramework: Framework? {
+        didSet {
+            isShowingDetailView = true
+        }
+    }
+}
