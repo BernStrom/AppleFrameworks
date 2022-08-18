71d08ab157631a41f50846cd3d12450898c4a772
Bern <bernnystrom@gmail.com>
2022-08-18 08:36:31 +0800

    💄 (new): Add initial grid view of mock frameworks
    Added an intital vertical grid of 3 columns with some custom mock frameworks views for testing out the app view layout.

--- AppleFrameworks/FrameworkGridView.swift
+++ AppleFrameworks/FrameworkGridView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    let columns = Array(repeating: GridItem(.flexible()), count: 3)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        LazyVGrid(columns: columns) {
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+        }
     }
 }
 
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
+    }
+}
+
+struct FrameworkTitleView: View {
+    let name: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            
+            Text(name)
+                .font(.title2.weight(.semibold))
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
     }
 }
