f04135d7b0ea557b4fcf45c98dab27d10c1b7c8f
Bern <bernnystrom@gmail.com>
2022-08-18 10:17:57 +0800

    💄 (new): Add reusable button view
    Created a reusable button with custom modifiers and dynamic title for its text.

---
+++ AppleFrameworks/AFButton.swift
@@ -0,0 +1,27 @@
+//
+//  AFButton.swift
+//  AppleFrameworks
+//
+//  Created by Bern N on 8/18/22.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.title2.weight(.semibold))
+            .frame(width: 280, height: 50)
+            .background(.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+struct AFButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AFButton(title: "Test Title")
+    }
+}
---
+++ AppleFrameworks/FrameworkDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkDetailView.swift
+//  AppleFrameworks
+//
+//  Created by Bern N on 8/18/22.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkDetailView()
+    }
+}
