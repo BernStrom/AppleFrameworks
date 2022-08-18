9c2b5c1536e4958b3a4d662502b291cd6f3d3257
Bern <bernnystrom@gmail.com>
2022-08-18 10:24:25 +0800

    🚧 (chore): Set initial layout structure for the framework detail screen
    Added the initial layout structure with some sample framework data for building & testing the framework detail screen. This commit is a work in progress and some of the view functionality might still be missing.

--- AppleFrameworks/FrameworkDetailView.swift
+++ AppleFrameworks/FrameworkDetailView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct FrameworkDetailView: View {
+    var framework: Framework
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .padding()
+            
+            Spacer()
+            
+            FrameworkTitleView(framework: framework)
+            
+            Text(framework.description)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                AFButton(title: "Learn More")
+            }
+        }
     }
 }
 
+
 struct FrameworkDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        FrameworkDetailView()
+        FrameworkDetailView(framework: MockData.sampleFramework)
+            .preferredColorScheme(.dark)
     }
 }
