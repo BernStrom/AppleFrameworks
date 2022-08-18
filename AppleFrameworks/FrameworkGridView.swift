63dabeb4c8f1f2b35e38bf569aa71ccc646e8376
Bern <bernnystrom@gmail.com>
2022-08-18 09:23:15 +0800

    💄 (new): Wrap frameworks in a scrollable navigation and render each framwork object dynamically
    Added a navigation & scroll view around the app layout grid and dynamically display each framework image & name through a ForEach loop. Improved the layout of the frameworks by adding some padding so they are more spaced apart.

--- AppleFrameworks/FrameworkGridView.swift
+++ AppleFrameworks/FrameworkGridView.swift
@@ -11,13 +11,15 @@ struct FrameworkGridView: View {
     let columns = Array(repeating: GridItem(.flexible()), count: 3)
     
     var body: some View {
-        LazyVGrid(columns: columns) {
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle(" Frameworks")
         }
     }
 }
@@ -25,23 +27,24 @@ struct FrameworkGridView: View {
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
+            .preferredColorScheme(.dark)
     }
 }
 
 struct FrameworkTitleView: View {
-    let name: String
-    let imageName: String
+    let framework: Framework
     
     var body: some View {
         VStack {
-            Image(imageName)
+            Image(framework.imageName)
                 .resizable()
                 .frame(width: 90, height: 90)
             
-            Text(name)
+            Text(framework.name)
                 .font(.title2.weight(.semibold))
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
         }
+        .padding()
     }
 }
