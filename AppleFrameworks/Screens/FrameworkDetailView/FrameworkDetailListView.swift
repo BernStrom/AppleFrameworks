588119b34251ad265f6c495f5cf72551520f66be
Bern <bernnystrom@gmail.com>
2022-09-03 04:41:18 +0800

    ♻️ (refactor): Update Learn More button to a Link
    Refactored the previous Learn More button in the framework detail view to use the built-in SwiftUI Link that was made available

--- AppleFrameworks/Screens/FrameworkDetailView/FrameworkDetailListView.swift
+++ AppleFrameworks/Screens/FrameworkDetailView/FrameworkDetailListView.swift
@@ -19,20 +19,10 @@ struct FrameworkDetailListView: View {
             
             Spacer()
             
-            Button {
-                viewModel.isShowingSafariView = true
-            } label: {
+            Link(destination: URL(string: viewModel.framework.urlString) ?? URL(string: "www.apple.com/notfound")!) {
                 Label("Learn More", systemImage: "book.fill")
             }
-            .buttonStyle(.bordered)
-            .controlSize(.large)
-            .buttonBorderShape(.roundedRectangle(radius: 15))
-            .tint(.green)
-            .padding(.bottom, 60)
-        }
-        .fullScreenCover(isPresented: $viewModel.isShowingSafariView) {
-            SafariView(url: URL(string: viewModel.framework.urlString) ?? URL(string: "www.apple.com/notfound")!)
-                .ignoresSafeArea()
+            .modifier(StandardLinkStyle())
         }
     }
 }
